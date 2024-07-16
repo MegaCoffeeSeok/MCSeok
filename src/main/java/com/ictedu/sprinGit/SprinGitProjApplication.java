b2476f14df76c27ac92209e055e511faf88bb43c
ChoHyunSeok <jo124635@naver.com>
2024-07-16 11:53:16 +0900

    Git학습용 스프링 프로젝트 생성

---
+++ src/main/java/com/ictedu/sprinGit/SprinGitProjApplication.java
@@ -0,0 +1,13 @@
+package com.ictedu.sprinGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinGitProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinGitProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ictedu/sprinGit/SprinGitProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictedu.sprinGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinGitProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
