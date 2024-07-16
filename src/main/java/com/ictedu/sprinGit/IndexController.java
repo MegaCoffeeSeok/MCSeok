95dd5efd4d0c62221135324d4d43ea72811f60eb
ChoHyunSeok <jo124635@naver.com>
2024-07-16 12:08:40 +0900

    "/" 라우팅용 IndexController 추가

---
+++ src/main/java/com/ictedu/sprinGit/IndexController.java
@@ -0,0 +1,11 @@
+package com.ictedu.sprinGit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+
+	@GetMapping("/")
+	public String index(){
+		return 
+	}
+}
